a6b402072dcdfc37282b7b5c8225aec9dc5a8925
姚尧 <yaogaoyu@gmail.com>
2019-05-08 00:22:02 +0800

    添加首页列表Demo代码。

--- src/pages/IndexPage/index.jsx
+++ src/pages/IndexPage/index.jsx
@@ -1,21 +1,54 @@
 import { Fragment } from 'react';
 import { View } from 'core/base';
 import { ViewCls, ServiceWired } from 'core/decorator';
-import { NavBar } from 'antd-mobile';
+import { NavBar, ListView } from 'antd-mobile';
 // import Link from 'components/Link';
 import { Link } from 'react-router-dom';
 import IndexService from './IndexService';
 import './index.less';
 
+const data = [
+    {
+        title: 'Meet hotel',
+        des: '不是所有的兼职汪都需要风吹日晒',
+    },
+    {
+        title: 'McDonald\'s invites you',
+        des: '不是所有的兼职汪都需要风吹日晒',
+    },
+    {
+        title: 'Eat the week',
+        des: '不是所有的兼职汪都需要风吹日晒',
+    },
+];
+
+/* eslint-disable */
+const ListBody = (props) => {
+    return (
+        <div className="am-list-body my-body">
+            <span style={{ display: 'none' }}>you can custom body wrap element</span>
+            {props.children}
+        </div>
+    );
+};
+/* eslint-enable */
+
 export default
 @ViewCls('list')
 class Index extends View {
     @ServiceWired(IndexService)
-    service;
+    service
 
-    componentWillMount() {
-        console.log(this.props);
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: true,
+        };
     }
+
+    #onEndReached = () => {
+        console.log('onEndReached');
+    };
 
     render() {
         return (
@@ -26,9 +59,39 @@ class Index extends View {
                         <Link key="1" className="nav-right link" to="/collections">收藏夹</Link>,
                     ]}
                 >
-                    YYBR
+                    书籍列表
                 </NavBar>
-
+                <ListView
+                    ref={(el) => {
+                        this.lv = el;
+                    }}
+                    dataSource={data}
+                    renderFooter={() => {
+                        return (
+                            <div style={{ padding: 30, textAlign: 'center' }}>
+                                {this.state.isLoading ? 'Loading...' : 'Loaded'}
+                            </div>
+                        );
+                    }}
+                    renderSectionHeader={(sectionData) => {
+                        console.log(sectionData);
+                        return <div>Task</div>;
+                    }}
+                    renderBodyComponent={() => {
+                        return <ListBody />;
+                    }}
+                    // renderRow={}
+                    // renderSeparator={separator}
+                    style={{
+                        // height: this.state.height,
+                        overflow: 'auto',
+                    }}
+                    pageSize={4}
+                    onScroll={() => { console.log('scroll'); }}
+                    scrollRenderAheadDistance={500}
+                    onEndReached={this.#onEndReached}
+                    onEndReachedThreshold={10}
+                />
             </Fragment>
         );
     }
