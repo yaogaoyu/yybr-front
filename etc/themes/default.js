078d0fcf310eee7346f315e9847a2f318c205edd
姚尧 <yaoyao2@douyu.tv>
2019-05-07 17:39:33 +0800

    添加样式文件.

---
+++ etc/themes/default.js
@@ -0,0 +1,41 @@
+module.exports = {
+    'hd': '1px', // 基本单位
+
+    // 支付宝钱包默认主题
+    // 'https'://github.com/ant-design/ant-design-mobile/wiki/设计变量表及命名规范
+
+    // 色彩
+    // ---
+    // 文字色
+    'color-text-base': '#000',                  // 基本
+    'color-text-base-inverse': '#fff',          // 基本 - 反色
+    'color-text-secondary': '#a4a9b0',          // 辅助色
+    'color-text-placeholder': '#bbb',           // 文本框提示
+    'color-text-disabled': '#bbb',              // 失效
+    'color-text-caption': '#888',               // 辅助描述
+    'color-text-paragraph': '#333',             // 段落
+
+    // 背景色
+    'fill-base': '#fff',                           // 组件默认背景
+    'fill-body': '#f5f5f9',                        // 页面背景
+    'fill-tap': '#ddd',                            // 组件默认背景 - 按下
+    'fill-disabled': '#ddd',                       // 通用失效背景
+    'fill-mask': 'rgba(0, 0, 0, 0.4)',              // 遮罩背景
+    'color-icon-base': '#ccc',                      // 许多小图标的背景，比如一些小圆点，加减号
+    'fill-grey': '#f7f7f7',
+
+    // 透明度
+    'opacity-disabled': '0.3',   // switch checkbox radio 等组件禁用的透明度
+
+    // 全局/品牌色
+    'brand-primary': '#108ee9',
+    'brand-primary-tap': '#0e80d2',
+    'brand-success': '#6abf47',
+    'brand-warning': '#ffc600',
+    'brand-error': '#f4333c',
+    'brand-important': '#ff5b05',  // 用于小红点
+    'brand-wait': '#108ee9',
+
+    // 边框色
+    'border-color-base': '#ddd'
+}
